f4ce97f186bf662155be1087af426b6405329b7d
juechen-zzz <240553516@qq.com>
2019-05-13 09:09:51 +0800

    add 47 48

---
+++ 47.Permutations II.py
@@ -0,0 +1,26 @@
+'''
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+'''
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        ans = [[]]
+        for n in nums:
+            new_ans = []
+            for l in ans:
+                for i in range(len(l)+1):
+                    new_ans.append(l[:i]+[n]+l[i:])
+                    if i<len(l) and l[i]==n: 
+                        break              #handles duplication
+            ans = new_ans
+        return ans
