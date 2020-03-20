f7da966c8662e6156e316f4e1161bf426c7af310
juechen-zzz <240553516@qq.com>
2020-03-21 00:13:07 +0800

    add 15 16

---
+++ C++(Leetcode)/0015_3Sum/main.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  0015_3Sum
+//
+//  Created by 倪浩鹏 on 2020/3/20.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+/*
+ Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+ Note:
+
+ The solution set must not contain duplicate triplets.
+
+ Example:
+
+ Given array nums = [-1, 0, 1, 2, -1, -4],
+
+ A solution set is:
+ [
+   [-1, 0, 1],
+   [-1, -1, 2]
+ ]
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void PrintVector1(vector<int> &v){
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << (*it) << " ";
+    }
+    cout << endl;
+}
+
+void PrintVector2(vector<vector<int>> &v){
+    cout << "[" << endl;
+    for (int i = 0; i < v.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < v[i].size(); j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+    cout << "]" << endl;
+}
+
+
+
+vector<vector<int>> threeSum(vector<int>& nums){
+    vector<vector<int>> ans;
+    // 元素个数小于3直接返回空
+    if (nums.size() < 3) {return ans;}
+    sort(nums.begin(), nums.end());
+    int len = (int)nums.size();
+    
+    for (int i = 0; i < len - 2; i++) {
+        if (nums[i] > 0) {break;}
+        // 排除会出现完全相同的组合可能[-1, -1, 0, 1]这种，把第二个-1跳过
+        if (i > 0 && nums[i] == nums[i-1]) {continue;}
+        
+        int l = i + 1;
+        int r = len - 1;
+        // 左右同时往中间靠
+        while (l < r) {
+            int total = nums[i] + nums[l] + nums[r];
+            if (total < 0) {l++;}
+            else if (total > 0) {r--;}
+            else {
+                ans.push_back(vector<int> {nums[i], nums[l], nums[r]});
+                // 左右出现重复值的时候就向中间移一位
+                while (l < r && nums[l] == nums[l + 1]) {l++;}
+                while (l < r && nums[r] == nums[r - 1]) {r--;}
+                l++;
+                r--;
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main(int argc, const char * argv[]) {
+    int s[] = {-1, 0, 1, 2, -1, -4};
+    vector<int> ss(s, s + 6);
+    vector<vector<int>> result1;
+    result1 = threeSum(ss);
+    PrintVector2(result1);
+    return 0;
+}
---
+++ C++(Leetcode)/0016_3Sum Closest/main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  0016_3Sum Closest
+//
+//  Created by 倪浩鹏 on 2020/3/20.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+/*
+ Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+ Example:
+
+ Given array nums = [-1, 2, 1, -4], and target = 1.
+
+ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//void PrintVector1(vector<int> &v){
+//    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+//        cout << (*it) << " ";
+//    }
+//    cout << endl;
+//}
+
+int threeSumClosest(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int ans = nums[0] + nums[1] + nums[2];
+    int len = (int)nums.size();
+    for (int i = 0; i < len - 2; i++) {
+        int l = i + 1;
+        int r = len - 1;
+        while (l < r) {
+            int total = nums[i] + nums[l] + nums[r];
+            if (abs((int)(total - target)) < abs((int)(ans - target))) {ans = total;}
+            
+            if (total < target) {l++;}
+            else if (total > target) {r--;}
+            else {
+                ans = target;
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+int main(int argc, const char * argv[]) {
+    int s[] = {0, 2, 1, -3};
+    vector<int> ss(s, s + 4);
+    cout << threeSumClosest(ss, 1) << endl;
+    cout << abs(-2);
+    return 0;
+}
