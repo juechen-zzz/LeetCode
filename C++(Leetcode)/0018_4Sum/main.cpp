024a3f392e9179e16dfc4a8aad51a16b125b80a9
juechen-zzz <240553516@qq.com>
2020-03-23 01:34:32 +0800

    add 18 19

---
+++ C++(Leetcode)/0018_4Sum/main.cpp
@@ -0,0 +1,89 @@
+//
+//  main.cpp
+//  0018_4Sum   解题思路同15
+//
+//  Created by 倪浩鹏 on 2020/3/23.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+/*
+ Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+ Note:
+
+ The solution set must not contain duplicate quadruplets.
+
+ Example:
+
+ Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+ A solution set is:
+ [
+   [-1,  0, 0, 1],
+   [-2, -1, 1, 2],
+   [-2,  0, 0, 2]
+ ]
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void PrintVector1(vector<int> &v){
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << (*it) << " ";
+    }
+    cout << endl;
+}
+
+void PrintVector2(vector<vector<int>> &v){
+    cout << "[" << endl;
+    for (int i = 0; i < v.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < v[i].size(); j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+    cout << "]" << endl;
+}
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> ans;
+    if (nums.size() < 4) {return ans;}
+    sort(nums.begin(), nums.end());
+    int len = (int)nums.size();
+    for (int i = 0; i < len - 3; i++) {
+        if (i > 0 && nums[i] == nums[i-1]) {continue;}
+        for (int j = i + 1; j < len - 2; j++) {
+            if (j > i+1 && nums[j] == nums[j-1]) {continue;}
+            int l = j + 1;
+            int r = len - 1;
+            while (l < r) {
+                int total = nums[i] + nums[j] + nums[l] + nums[r];
+                if (total < target) {l++;}
+                else if (total > target) {r--;}
+                else {
+                    ans.push_back(vector<int> {nums[i], nums[j], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) {l++;}
+                    while (l < r && nums[r] == nums[r - 1]) {r--;}
+                    l++;
+                    r--;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main(int argc, const char * argv[]) {
+    int a[] = {1,-2,-5,-4,-3,3,3,5};
+    vector<int> ss(a, a + 8);
+    PrintVector1(ss);
+    
+    vector<vector<int>> result;
+    result = fourSum(ss, -11);
+    PrintVector2(result);
+    return 0;
+}
---
+++ C++(Leetcode)/0019_Remove Nth Node From End of List/main.cpp
@@ -0,0 +1,64 @@
+//
+//  main.cpp
+//  0019_Remove Nth Node From End of List
+//
+//  Created by 倪浩鹏 on 2020/3/23.
+//  Copyright © 2020 nihaopeng. All rights reserved.
+//
+
+/*
+ Given a linked list, remove the n-th node from the end of list and return its head.
+
+ Example:
+
+ Given linked list: 1->2->3->4->5, and n = 2.
+
+ After removing the second node from the end, the linked list becomes 1->2->3->5.
+ Note:
+
+ Given n will always be valid.
+ */
+
+
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // 首先，计算链表长度，单链表无法反向遍历
+        ListNode *tmp = head;
+        int len = 0;
+        while (tmp){
+            tmp = tmp->next;
+            len++;
+        }
+        
+        // 当要删除的就是头节点的情况
+        if (len == n) {
+            head = head->next;
+            return head;
+        }
+        
+        // 正常遍历到要删除的节点前一个节点
+        tmp = head;
+        int i = 1;
+        while(i < len - n){
+            tmp = tmp->next;
+            i++;
+        }
+        
+        // 保存删除的节点
+        ListNode* del = tmp->next;
+        tmp->next = tmp->next->next;
+        delete(del);
+        return head;
+    }
+};
