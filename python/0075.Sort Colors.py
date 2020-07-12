c4466ca3f225d7f8551c2728f254e83c6c517dbb
juechen-zzz <240553516@qq.com>
2020-07-12 23:28:51 +0800

    review python leetcode until 75

--- python/0075.Sort Colors.py
+++ python/0075.Sort Colors.py
@@ -13,15 +13,22 @@ Output: [0,0,1,1,2,2]
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        b = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
-                if nums[j] > nums[j+1]:
-                    b = nums[j]
-                    nums[j] = nums[j+1]
-                    nums[j+1] = b
-                    
-                    
+        '''
+        荷兰三色旗问题解
+        '''
+        # 对于所有 idx < p0 : nums[idx < p0] = 0
+        # curr是当前考虑元素的下标
+        p0 = curr = 0
+        # 对于所有 idx > p2 : nums[idx > p2] = 2
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1
