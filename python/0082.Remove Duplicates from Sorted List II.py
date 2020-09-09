8ef413bbfd66f4256ceb2480863186fcf3631479
juechen-zzz <240553516@qq.com>
2020-09-09 19:24:32 +0800

    restart to 90

--- python/0082.Remove Duplicates from Sorted List II.py
+++ python/0082.Remove Duplicates from Sorted List II.py
@@ -20,10 +20,12 @@ Output: 2->3
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         if not head: return None
+        # defaultdict还可以被用来计数，将default_factory设为int即可
+        # 字符串中的字母第一次出现时，字典中没有该字母，default_factory函数调用int()为其提供一个默认值0,加法操作将计算出每个字母出现的次数
         freq = collections.defaultdict(int)
         nums = []
         curr = head
-
+        
         while curr:
             freq[curr.val] += 1
             nums.append(curr.val)
@@ -36,5 +38,4 @@ class Solution:
             if freq[n] == 1:
                 curr.next = ListNode(n)
                 curr = curr.next
-        
         return rhead.next
