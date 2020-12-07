eb6532de9a5f529ce04fafc6768d06bad0466b65
juechen-zzz <240553516@qq.com>
2020-12-07 21:21:04 +0800

    add until 375 in 2020-12-07

---
+++ python/0375.Guess Number Higher or Lower II.py
@@ -0,0 +1,31 @@
+"""
+    猜数字，猜n就要付n元，问要保证猜到，最少要多少钱
+
+Input: n = 10
+Output: 16
+Explanation: The winning strategy is as follows:
+- The range is [1,10]. Guess 7.
+    - If this is my number, your total is $0. Otherwise, you pay $7.
+    - If my number is higher, the range is [8,10]. Guess 9.
+        - If this is my number, your total is $7. Otherwise, you pay $9.
+        - If my number is higher, it must be 10. Guess 10. Your total is $7 + $9 = $16.
+        - If my number is lower, it must be 8. Guess 8. Your total is $7 + $9 = $16.
+    - If my number is lower, the range is [1,6]. Guess 3.
+        - If this is my number, your total is $7. Otherwise, you pay $3.
+        - If my number is higher, the range is [4,6]. Guess 5.
+            - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $5.
+            - If my number is higher, it must be 6. Guess 6. Your total is $7 + $3 + $5 = $15.
+            - If my number is lower, it must be 4. Guess 4. Your total is $7 + $3 + $5 = $15.
+        - If my number is lower, the range is [1,2]. Guess 1.
+            - If this is my number, your total is $7 + $3 = $10. Otherwise, you pay $1.
+            - If my number is higher, it must be 2. Guess 2. Your total is $7 + $3 + $1 = $11.
+The worst case in all these scenarios is that you pay $16. Hence, you only need $16 to guarantee a win.
+"""
+
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        dp = [[0 for _ in range(n+2)] for _ in range(n+2)]
+        for low in range(n, 0, -1):
+            for high in range(low+1, n+1):
+                dp[low][high] = min(num + max(dp[low][num-1], dp[num+1][high]) for num in range(low, high + 1))
+        return dp[1][n]
