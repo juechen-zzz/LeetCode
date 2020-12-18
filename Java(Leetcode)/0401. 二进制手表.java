18cc0c1c83956409fe0cf41ee83784e99f27c941
juechen-zzz <240553516@qq.com>
2020-12-18 22:26:07 +0800

    add in 2020-12-18

---
+++ Java(Leetcode)/0401. 二进制手表.java
@@ -0,0 +1,41 @@
+/*
+二进制手表顶部有 4 个 LED 代表 小时（0-11），底部的 6 个 LED 代表 分钟（0-59）。
+
+每个 LED 代表一个 0 或 1，最低位在右侧。
+
+示例：
+
+输入: n = 1
+返回: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+ 
+
+提示：
+输出的顺序没有要求。
+小时不会以零开头，比如 “01:00” 是不允许的，应为 “1:00”。
+分钟必须由两位数组成，可能会以零开头，比如 “10:2” 是无效的，应为 “10:02”。
+超过表示范围（小时 0-11，分钟 0-59）的数据将会被舍弃，也就是说不会出现 "13:00", "0:61" 等时间。
+*/
+
+class Solution {
+    List<String> res = new ArrayList<>();
+    int[] dic = new int[]{1, 2, 4, 8, 1, 2, 4, 8, 16, 32};
+    
+    public List<String> readBinaryWatch(int num) {
+        dfs(num, 0, 0, 0);
+        return res;
+    }
+
+    void dfs(int count, int h, int m, int index){
+        if (count == 0){
+            res.add(h + ":" + (m > 9 ? m : "0" + m));
+        }
+        for (int i = index; i < 10; i++){
+            if (i < 4 && h + dic[i] < 12){
+                dfs(count - 1, h + dic[i], m, i + 1);
+            }
+            if (i >= 4 && m + dic[i] < 60){
+                dfs(count - 1, h, m + dic[i], i + 1);
+            }
+        }
+    }
+}
---
+++ Java(Leetcode)/0402. 移掉K位数字.java
@@ -0,0 +1,79 @@
+/*
+给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+
+注意:
+num 的长度小于 10002 且 ≥ k。
+num 不会包含任何前导零。
+
+示例 1 :
+输入: num = "1432219", k = 3
+输出: "1219"
+解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+*/
+
+// 1 回溯（超时）
+class Solution {
+    List<Integer> res = new ArrayList<>();
+
+    public String removeKdigits(String num, int k) {
+        char[] nums = num.toCharArray();
+        backtrack(nums, k, 0);
+        return Collections.min(res).toString();
+    }
+
+    void backtrack(char[] nums, int count, int start){
+        if (count == 0) {
+            res.add(string2int(nums));
+            return;
+        }
+        for (int i = start; i < nums.length; i++){
+            char tmp = nums[i];
+            nums[i] = 'a';
+            backtrack(nums, count - 1, i + 1);
+            nums[i] = tmp;
+        }
+    }
+
+    public int string2int(char[] n){
+        int ans = 0;
+        for (char c : n){
+            if (Character.isDigit(c)){
+                ans = ans * 10 + Integer.parseInt(String.valueOf(c));
+            }
+        }
+        return ans;
+    }
+}
+
+// 2 栈
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        for (int i = 0; i < num.length(); i++){
+            char n = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > n){
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(n);
+        }
+
+        // 特殊情况：当数为递增时，不走while，要删除最后的K个数
+        for (int i = 0; i < k; ++i) {
+            deque.pollLast();
+        }
+
+        StringBuilder ans = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ans.append(digit);
+        }
+        return ans.length() == 0 ? "0" : ans.toString();
+    }
+}
+
