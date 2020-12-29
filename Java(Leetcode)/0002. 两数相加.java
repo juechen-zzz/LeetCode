3c624da755ae41cf99ca9ed0517fbc42f502f1d4
juechen-zzz <240553516@qq.com>
2020-12-29 21:36:11 +0800

    add in 2020-12-29

---
+++ Java(Leetcode)/0002. 两数相加.java
@@ -0,0 +1,52 @@
+/* 
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(carry % 10);
+            carry = carry / 10;
+            curr = curr.next;
+        }
+
+        if (carry == 1) {
+            curr.next = new ListNode(1);
+            curr = curr.next;
+        }
+        return head.next;
+    }
+}
