5f6038d797715bbf1401616e04f23f59c8c4fa4e
juechen-zzz <240553516@qq.com>
2021-02-22 22:59:21 +0800

    add in 2021-2-22

---
+++ Java(Leetcode)/0179. 最大数.java
@@ -0,0 +1,40 @@
+/* 
+给定一组非负整数 nums，重新排列它们每个数字的顺序（每个数字不可拆分）使之组成一个最大的整数。
+
+注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+
+示例 1：
+输入：nums = [10,2]
+输出："210"
+示例 2：
+
+输入：nums = [3,30,34,5,9]
+输出："9534330"
+示例 3：
+
+输入：nums = [1]
+输出："1"
+示例 4：
+
+输入：nums = [10]
+输出："10"
+
+ */
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        StringBuilder ans = new StringBuilder();
+        for (String s : strs) {
+            if (ans.toString().equals("0")) {ans.delete(0, 1);}
+            ans.append(s);
+        }
+
+        return ans.toString();
+    }
+}
---
+++ Java(Leetcode)/0187. 重复的DNA序列.java
@@ -0,0 +1,37 @@
+/* 
+所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+
+编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
+
+ 
+
+示例 1：
+
+输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+输出：["AAAAACCCCC","CCCCCAAAAA"]
+示例 2：
+
+输入：s = "AAAAAAAAAAAAA"
+输出：["AAAAAAAAAA"]
+
+ */
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> ans = new ArrayList<>();
+        Map<String, Integer> count = new HashMap<>();
+
+        for (int i = 0; i < s.length() - 9; i++) {
+            String tmp = s.substring(i , i + 10);
+            if (count.containsKey(tmp) && count.get(tmp) == 1) {
+                ans.add(tmp);
+                count.put(tmp, 2);
+            }
+            else if (!count.containsKey(tmp)) {
+                count.put(tmp, 1);
+            }
+        }
+
+        return ans;
+    }
+}
