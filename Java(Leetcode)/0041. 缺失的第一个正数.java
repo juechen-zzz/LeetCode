31c895f973c599fe4ebd0d5210f97c41bfe3f5f8
juechen-zzz <240553516@qq.com>
2021-03-10 18:31:03 +0800

    add in 2021-3-10 (1)

--- Java(Leetcode)/0041. 缺失的第一个正数.java
+++ Java(Leetcode)/0041. 缺失的第一个正数.java
@@ -1,11 +1,7 @@
 /* 
 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
 
- 
-
 进阶：你可以实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案吗？
-
- 
 
 示例 1：
 
@@ -22,6 +18,7 @@
 
  */
 
+// 1 set
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int len = nums.length;
@@ -36,5 +33,29 @@ class Solution {
         }
 
         return len + 1;
+    }
+}
+
+// HashMap
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 }
