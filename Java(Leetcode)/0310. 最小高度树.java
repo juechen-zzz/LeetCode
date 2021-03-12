8a1ccd6256d72cf05bcc1aa75bac577741f2d954
juechen-zzz <240553516@qq.com>
2021-03-12 22:00:54 +0800

    add in 2021-3-12

---
+++ Java(Leetcode)/0310. 最小高度树.java
@@ -0,0 +1,71 @@
+/* 
+树是一个无向图，其中任何两个顶点只通过一条路径连接。 换句话说，一个任何没有简单环路的连通图都是一棵树。
+
+给你一棵包含 n 个节点的树，标记为 0 到 n - 1 。给定数字 n 和一个有 n - 1 条无向边的 edges 列表（每一个边都是一对标签），其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条无向边。
+
+可选择树中任何一个节点作为根。当选择节点 x 作为根节点时，设结果树的高度为 h 。在所有可能的树中，具有最小高度的树（即，min(h)）被称为 最小高度树 。
+
+请你找到所有的 最小高度树 并按 任意顺序 返回它们的根节点标签列表。
+
+树的 高度 是指根节点和叶子节点之间最长向下路径上边的数量。
+
+示例 1：
+
+输入：n = 4, edges = [[1,0],[1,2],[1,3]]
+输出：[1]
+解释：如图所示，当根是标签为 1 的节点时，树的高度是 1 ，这是唯一的最小高度树。
+
+ */
+
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> ans = new ArrayList<>();
+
+        // 构建邻接表和各个节点的度
+        int[] degree = new int[n];
+        List<List<Integer>> adjency = new ArrayList<>();
+        for (int i = 0; i < n; i++) {adjency.add(new ArrayList<>());}
+        for (int[] edge : edges) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            adjency.get(edge[0]).add(edge[1]);
+            adjency.get(edge[1]).add(edge[0]);
+        }
+
+        // 一度节点(叶子节点)入队
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1) {
+                queue.offer(i);
+            }
+        }
+
+        // 单独处理只有一个节点的情况
+        int residue = n;
+        if (residue == 1) {ans.add(0);}
+
+        // 剩余的节点不是一个或两个,将叶子节点(度为1)一层一层剥离,最后暴露出来的就是根
+        // 最后的根节点只可能是一个或者两个
+        while (residue != 1 && residue != 2) {
+            int size = queue.size();
+            residue -= size;
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                for (int node : adjency.get(cur)) {
+                    if (degree[node] > 0) {
+                        degree[node]--;
+                    }
+                    if (degree[node] == 1) {
+                        queue.offer(node);
+                    }
+                }
+            }
+        }
+
+        // 队列中最后剩下的就是根
+        while (!queue.isEmpty()) {
+            ans.add(queue.poll());
+        }
+        return ans;
+    }
+}
---
+++ Java(Leetcode)/0312. 戳气球.java
@@ -0,0 +1,44 @@
+/* 
+有 n 个气球，编号为0 到 n - 1，每个气球上都标有一个数字，这些数字存在数组 nums 中。
+
+现在要求你戳破所有的气球。戳破第 i 个气球，你可以获得 nums[i - 1] * nums[i] * nums[i + 1] 枚硬币。 这里的 i - 1 和 i + 1 代表和 i 相邻的两个气球的序号。如果 i - 1或 i + 1 超出了数组的边界，那么就当它是一个数字为 1 的气球。
+
+求所能获得硬币的最大数量。
+
+ 
+
+示例 1：
+输入：nums = [3,1,5,8]
+输出：167
+解释：
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+示例 2：
+
+输入：nums = [1,5]
+输出：10
+
+ */
+
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        // rec[i][j] 表示[i,j]区间内的最大硬币数
+        int[][] rec = new int[n + 2][n + 2];
+
+        int[] val = new int[n + 2];
+        val[0] = val[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {val[i] = nums[i - 1];}
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j <= n + 1; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += (rec[i][k] + rec[k][j]);
+                    rec[i][j] = Math.max(rec[i][j], sum);
+                }
+            }
+        }
+        return rec[0][n + 1];
+    }
+}
