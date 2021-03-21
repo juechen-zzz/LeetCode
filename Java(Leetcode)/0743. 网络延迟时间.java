59d892c7d66666c0ae6e3e64af9d73068b0d1d38
juechen-zzz <240553516@qq.com>
2021-03-21 11:51:22 +0800

    test idea git

--- Java(Leetcode)/0743. 网络延迟时间.java
+++ Java(Leetcode)/0743. 网络延迟时间.java
@@ -25,12 +25,10 @@ class Solution {
             if (!graph.containsKey(edge[0])) {
                 graph.put(edge[0], new ArrayList<int[]>());
             }
-            graph.get(edge[0]).add(new int[]{edge[2], edge[1]});
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
         }
 
-        for (int node : graph.keySet()) {
-            Collections.sort(graph.get(node), (a, b) -> a[0] - b[0]);
-        }
+        for (int node : graph.keySet()) {Collections.sort(graph.get(node), (a, b) -> a[0] - b[0]);}
         for (int i = 1; i <= n; i++) {dist.put(i, Integer.MAX_VALUE);}
 
         dfs(graph, k, 0);
@@ -48,7 +46,7 @@ class Solution {
         dist.put(node, time);
         if (graph.containsKey(node)) {
             for (int[] info : graph.get(node)) {
-                dfs(graph, info[1], time + info[0]);
+                dfs(graph, info[0], time + info[1]);
             }
         }
     }
