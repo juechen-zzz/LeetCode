b495329e9e5c990cbd62397e1b22fd8ed3ec350c
juechen-zzz <240553516@qq.com>
2021-05-07 09:12:11 +0800

    add in 2021-5-7

---
+++ Java(Leetcode)/0709. 转换成小写字母.java
@@ -0,0 +1,31 @@
+/* 
+实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+
+ 
+
+示例 1：
+
+输入: "Hello"
+输出: "hello"
+示例 2：
+
+输入: "here"
+输出: "here"
+示例 3：
+
+输入: "LOVELY"
+输出: "lovely"
+
+ */
+
+class Solution {
+    public String toLowerCase(String str) {
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= 'A' && array[i] <= 'Z') {
+                array[i] += 32;
+            }
+        }
+        return new String(array);
+    }
+}
---
+++ Java(Leetcode)/0712. 两个字符串的最小ASCII删除和.java
@@ -0,0 +1,47 @@
+/* 
+给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
+
+示例 1:
+
+输入: s1 = "sea", s2 = "eat"
+输出: 231
+解释: 在 "sea" 中删除 "s" 并将 "s" 的值(115)加入总和。
+在 "eat" 中删除 "t" 并将 116 加入总和。
+结束时，两个字符串相等，115 + 116 = 231 就是符合条件的最小和。
+示例 2:
+
+输入: s1 = "delete", s2 = "leet"
+输出: 403
+解释: 在 "delete" 中删除 "dee" 字符串变成 "let"，
+将 100[d]+101[e]+101[e] 加入总和。在 "leet" 中删除 "e" 将 101[e] 加入总和。
+结束时，两个字符串都等于 "let"，结果即为 100+101+101+101 = 403 。
+如果改为将两个字符串转换为 "lee" 或 "eet"，我们会得到 433 或 417 的结果，比答案更大。
+
+ */
+
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            dp[i][s2.length()] = dp[i + 1][s2.length()] + s1.codePointAt(i);
+        }
+
+        for (int j = s2.length() - 1; j >= 0; j--) {
+            dp[s1.length()][j] = dp[s1.length()][j + 1] + s2.codePointAt(j);
+        }
+
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i + 1][j] + s1.codePointAt(i), dp[i][j + 1] + s2.codePointAt(j));
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
